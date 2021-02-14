49b056d5d7457dbd9e3b4cc46e6fb65a6061fe38
nissan <shubhasish012@gmail.com>
2021-02-15 01:36:06 +0600

    header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBixLhqDZuO76ywDHrB9596I9RJ-7ybIrQ",
+    authDomain: "myclone-467f4.firebaseapp.com",
+    projectId: "myclone-467f4",
+    storageBucket: "myclone-467f4.appspot.com",
+    messagingSenderId: "631813821349",
+    appId: "1:631813821349:web:c734e2aa8314fe5f094e4c",
+    measurementId: "G-0BYH8P3JJP"
+  };
