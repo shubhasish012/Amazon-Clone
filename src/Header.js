78898fe771f914eb7cc2e93d04f1bb646182b7c6
nissan <shubhasish012@gmail.com>
2021-02-16 13:03:41 +0600

    context api

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="header">
       <Link to="/">
@@ -50,7 +53,7 @@ function Header() {
           <div className="header_optionBasket">
             <ShoppingBasketIcon />
             <span className="header_optionLineTwo header_basketCount">
-              0
+              {basket?.length}
             </span>
           </div>
         </Link>
